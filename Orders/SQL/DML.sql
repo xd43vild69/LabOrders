d8456547c37e8475e54c59e460230792280f9653
xd43vild69 <xd43vild69@gmail.com>
2019-04-13 15:20:20 -0500

    Fix unit test

--- Orders/SQL/DML.sql
+++ Orders/SQL/DML.sql
@@ -1,8 +1,8 @@
 insert into Patient (FirstName, LastName, MedicalHistory, Gender, Telephone, EmergencyContactTelephone, BirthDay)
-values ('David', 'Gomez', 'Sin Historial', 'M', '3151515', '12335456', '2019-12-19')
+values ('Patient1', 'Gomez', 'Sin Historial', 'M', '3151515', '12335456', '2019-12-19')
 
 insert into Patient (FirstName, LastName, MedicalHistory, Gender, Telephone, EmergencyContactTelephone, BirthDay)
-values ('Ana', 'Gomez', 'Sin Historial', 'M', '3151515', '12335456', '2019-12-19')
+values ('Patient2', 'Gomez', 'Sin Historial', 'M', '3151515', '12335456', '2019-12-19')
 
 insert into OrderType (NameType) values ('MEDICAMENTO')
 insert into OrderType (NameType) values ('CIRUGIA')
@@ -11,3 +11,7 @@ insert into OrderType (NameType) values ('TRATAMIENTO')
 insert into OrderState (NameOrderState) values ('ORDENADO')
 insert into OrderState (NameOrderState) values ('CANCELADO')
 insert into OrderState (NameOrderState) values ('SUMINISTRADO')
+
+insert into OrderPriority (NameOrderPriority) values ('ALTA')
+insert into OrderPriority (NameOrderPriority) values ('MEDIA')
+insert into OrderPriority (NameOrderPriority) values ('BAJA')
--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# LabOrders
-Dapper .Net Framework - .NET Core - 
+# LabOrders TEST 2019
+ .Net Framework - .NET Core - Dapper
+
+
+## Architecture
+
+- Carvajal.DOMAIN
+	- BAL: Business logic layer.
+	- DTO: Data transfer objects.
+- Carvajal.DATA 
+	- SAL: Data acess layer.
+- Carvajal.API
+	- API: Web API.
+- Carvajal.WEB: 
+	- APP: Application client.
+- Carvajal.TEST: 
+	- UnitTest: Unit Test project.	
+
+## TOOLS
+
+- .NET Framework BACKEND : API - DAL - SAL - DTOS
+- .NET Core 2.1 FRONTEND : APP-WEB 
+- JS - Jquery - CSS - HTML5
+- Dapper
+- AutoFac
+- Microsoft SQL Server
+
+## How to run
+
+1. Change connectionStrings:
+- Carvajal.API/API
+
+
+2.1 Run DDL.sql scripts in SQL FOLDER
+2.2 Run DML.sql scripts in SQL FOLDER
+
+
+3. Set up Carvajal.API/API & Carvajal.WEB/APP on Multi start projects.
+
+					Run.
